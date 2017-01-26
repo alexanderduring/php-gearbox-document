f500c4686c208c579d0436665e87c225e32875c5
Alexander During <alexander.during@jobleads.de>
2017-01-26 13:57:57 +0100

    Added first implementation of Document class.

--- src/Document/Document.php
+++ src/Document/Document.php
@@ -4,5 +4,46 @@ namespace Gearbox\Document;
 
 class Document
 {
+    private $data = array();
 
+
+
+    public function __construct(array $data)
+    {
+        $this->data = $data;
+    }
+
+
+
+    /**
+     * Unsets a field in the document, if it exists.
+     *
+     * @param array $keys
+     * @return boolean
+     */
+    protected function delete($keys)
+    {
+        $level = &$this->data;
+        $lastKey = array_pop($keys);
+        $success = true;
+
+        foreach ($keys as $key) {
+            if (array_key_exists($key, $level)) {
+                if (!is_array($level[$key])) {
+                    $success = false;
+                    break;
+                }
+                $level = &$level[$key];
+            } else {
+                $success = false;
+                break;
+            }
+        }
+
+        if ($success) {
+            unset($level[$lastKey]);
+        }
+
+        return $success;
+    }
 }
